46844ca024e7285961e0d414f731db9953561fb9
ErezM4N <erezm4n@gmail.com>
2022-01-13 16:12:45 +0200

    End Of Section 4

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -3,6 +3,11 @@ import { Container, CssBaseline } from "@mui/material";
 import Catalog from "../../features/catalog/Catalog";
 import Header from "./Header";
 import { useState } from 'react';
+import { Route } from 'react-router-dom';
+import HomePage from '../../features/home/HomePage';
+import ProductDetails from '../../features/catalog/ProductDetails';
+import AboutPage from '../../features/about/AboutPage';
+import ContactPage from '../../features/contact/ContactPage';
 
 
 // const productsList = [
@@ -31,25 +36,21 @@ function App() {
   }
 
 
-
-
-
-
   return (
     <ThemeProvider theme={theme}>
-
       <CssBaseline />
-
       <Header darkMode={darkMode} handleThemeChange={handleThemeChange} />
       <Container>
-
-        <Catalog />
+        <Container>
+          <Route exact path='/' component={HomePage}/>
+          <Route exact path='/catalog' component={Catalog}/>
+          <Route path='/catalog/:id' component={ProductDetails}/>
+          <Route path='/about' component={AboutPage}/>
+          <Route path='/contact' component={ContactPage}/>
+          {/* <Catalog /> */}
+        </Container>
       </Container>
-
-
     </ThemeProvider>
-
-
   );
 }
 
--- client/src/app/layout/Header.tsx
+++ client/src/app/layout/Header.tsx
@@ -1,24 +1,105 @@
-import { AppBar, Toolbar, Typography } from "@mui/material";
+import { ShoppingCart } from "@mui/icons-material";
+import { AppBar, Badge, Box, IconButton, List, ListItem, Toolbar, Typography } from "@mui/material";
 import Switch from '@mui/material/Switch';
+import { NavLink } from "react-router-dom";
 
 const label = { inputProps: { 'aria-label': 'Switch demo' } };
 
 interface Props {
-    darkMode: boolean;
-    handleThemeChange: () => void;
-    //setDarkMode: () => void;
+  darkMode: boolean;
+  handleThemeChange: () => void;
 }
 
+const midLink = [
+  { title: 'catalog', path: '/catalog' },
+  { title: 'about', path: '/about' },
+  { title: 'contact', path: '/contact' },
+]
+
+const rightLinks = [
+  { title: 'login', path: '/login' },
+  { title: 'register', path: '/register' },
+]
+
+const navStyles = {
+  color: 'inherit',
+  textDecoration: 'none',
+  typography: 'h6',
+  '&:hover': {
+    color: 'grey.500'
+  },
+  '&.active': {
+    color: 'text.secondary'
+  }
+}
+
+
 export default function Header({ darkMode, handleThemeChange }: Props) {
-    //const darkMode = darkMode;
-    return (
-        <AppBar position="static" sx={{ mb: 4 }} >
-            <Toolbar>
-                <Typography variant="h6">
-                    RE-STORE
-                </Typography>
-                <Switch {...label} checked={darkMode} onChange={handleThemeChange} />
-            </Toolbar>
-        </AppBar>
-    )
+  //const darkMode = darkMode;
+  const navigateHandler = (path: string) => {
+    console.log(path);
+  }
+  return (
+    <AppBar position="static" sx={{ mb: 4 }} >
+      <Toolbar sx={{
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+
+      }}>
+        <Box display='flex' alignItems='center' margin='2px'>
+          <Typography
+            variant="h6"
+            component={NavLink}
+            to='/'
+            exact
+            sx={navStyles}>
+            RE-STORE
+          </Typography>
+          <Switch {...label} checked={darkMode} onChange={handleThemeChange} />
+        </Box>
+
+        {/* - */}
+
+
+          <List sx={{ display: 'flex' }}>
+            {midLink.map(({ title, path }) => (
+              <ListItem
+                //onClick={e => navigateHandler(path)}
+                component={NavLink}
+                to={path}
+                key={path}
+                sx={navStyles}
+              >
+                {title.toUpperCase()}
+              </ListItem>
+            ))}
+          </List>
+
+
+        {/* - */}
+
+        <Box sx={{ display: 'flex' }}>
+          <IconButton size="large" sx={{ color: 'inherit' }}>
+            <Badge badgeContent={4} color="secondary">
+              <ShoppingCart />
+            </Badge>
+          </IconButton>
+          <List sx={{ display: 'flex' }}>
+            {rightLinks.map(({ title, path }) => (
+              <ListItem
+                //onClick={e => navigateHandler(path)}
+                component={NavLink}
+                to={path}
+                key={path}
+                sx={navStyles}
+              >
+                {title.toUpperCase()}
+              </ListItem>
+            ))}
+          </List>
+        </Box>
+      </Toolbar>
+    </AppBar>
+  )
 }
